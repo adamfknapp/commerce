01ff06500216e497b7a88d46471ae451bae10282
Adam Knapp <adamfknapp@gmail.com>
2021-02-27 23:28:45 -0500

    start models

--- auctions/models.py
+++ auctions/models.py
@@ -1,18 +1,27 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from datetime import datetime
 
 
 class User(AbstractUser):
     pass
 
-class listings(model.Models):
-    description = models.CharField(max_length=500)
-    #category_id
-    #listed_by = 
-    min_bid = models.IntegerField(max_length=64)
-    time_create = models.DateTimeField()
-    time_end = models.DateTimeField()
-    isactive = models.BooleanField()
+class category(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class listing(models.Model):
+    title = models.CharField(max_length=20)
+    description = models.CharField(max_length=5000, default="None")
+    min_bid = models.IntegerField(default=1)
+    time_create = models.DateTimeField(default= datetime.now())
+    hours_to_end = models.IntegerField(default=1)
+    isactive = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"title: {self.title} | minumum bid: {self.min_bid} | is active: {self.isactive}"
 
 
 """
@@ -45,5 +54,4 @@ class Passenger(models.Model):
 
     def __str__(self):
         return f"{self.first} {self.last}"
-
 """
