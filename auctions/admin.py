cee707bd0c7f866c72eb978382ce49227f18f2fe
Adam Knapp <adamfknapp@gmail.com>
2021-02-28 22:29:02 -0500

    set up admin pannel

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import category, listing, bid, comment
 
 # Register your models here.
+# per requierment 19
+admin.site.register(category)
+admin.site.register(listing)
+admin.site.register(bid)
+admin.site.register(comment)
--- auctions/models.py
+++ auctions/models.py
@@ -5,8 +5,9 @@ from django.db import models
 class User(AbstractUser):
     pass
 
+
 class category(models.Model):
-    name = models.CharField(max_length=10, unique=True)
+    name = models.CharField(max_length=30, unique=True)
 
     def __str__(self):
         return f"{self.name}"
@@ -15,23 +16,24 @@ class category(models.Model):
 class listing(models.Model):
     title = models.CharField(max_length=20)
     description = models.TextField(max_length=5000, default="no description")
-    creator = models.CharField(max_length=10, unique=True)
-    photo =  models.URLField(max_length = 200, blank=True, null=True) 
-    category = models.ForeignKey(category, on_delete=models.CASCADE, related_name="listings", default= "None")
+    creator = models.CharField(max_length=10, unique=True, default="default_user")
+    photo = models.URLField(max_length=200, blank=True, null=True)
+    category = models.ForeignKey(category, on_delete=models.CASCADE,
+                                 related_name="listings", default="None")
     start_bid = models.DecimalField(max_digits=5, decimal_places=0, default=1)
     time_create = models.DateTimeField(auto_now_add=True)
-    active_time = models.IntegerField(default=1) #in hours
-    #Watchers
-    isactive = models.BooleanField(default=True)
+    active_time = models.IntegerField(default=1)  # in hours
+    close_early = models.BooleanField(default=False)  # requierment 15
 
     def __str__(self):
-        return f"title: {self.title} | minumum bid: {self.min_bid} | is active: {self.isactive}"
+        return f"title: {self.title} | is active: {self.isactive}"
 
 
 class bid(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
-    #bidder = request.user
-    listing = models.ForeignKey(listing, on_delete=models.CASCADE, related_name="bids")
+    # bidder = request.user
+    listing = models.ForeignKey(listing, on_delete=models.CASCADE,
+                                related_name="bids")
     bid = models.DecimalField(max_digits=5, decimal_places=0, default=1)
 
     def __str__(self):
@@ -40,9 +42,10 @@ class bid(models.Model):
 
 class comment(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
-    #bidder = request.user
-    listing = models.ForeignKey(listing, on_delete=models.CASCADE, related_name="comments")
-    comment = models.CharField(max_length=100) 
-    
+    # bidder = request.user
+    listing = models.ForeignKey(listing, on_delete=models.CASCADE,
+                                related_name="comments")
+    comment = models.CharField(max_length=100)
+
     def __str__(self):
         return f"listing: {self.listing} | bid: {self.comment}"
