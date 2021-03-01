dc8b81d74fc248a0a1331c0fcecee4916ae090d8
Adam Knapp <adamfknapp@gmail.com>
2021-02-28 20:31:19 -0500

    Update models.py

--- auctions/models.py
+++ auctions/models.py
@@ -1,58 +1,48 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from datetime import datetime
 
 
 class User(AbstractUser):
     pass
 
 class category(models.Model):
-    name = models.CharField(max_length=10)
+    name = models.CharField(max_length=10, unique=True)
 
     def __str__(self):
         return f"{self.name}"
 
+
 class listing(models.Model):
     title = models.CharField(max_length=20)
-    description = models.CharField(max_length=5000, default="None")
-    category = models.ForeignKey(category, on_delete=models.CASCADE, related_name="listings")
-    min_bid = models.IntegerField(default=1)
-    time_create = models.DateTimeField(default= datetime.now())
-    hours_to_end = models.IntegerField(default=1)
+    description = models.TextField(max_length=5000, default="no description")
+    creator = models.CharField(max_length=10, unique=True)
+    photo =  models.URLField(max_length = 200, blank=True, null=True) 
+    category = models.ForeignKey(category, on_delete=models.CASCADE, related_name="listings", default= "None")
+    start_bid = models.DecimalField(max_digits=5, decimal_places=0, default=1)
+    time_create = models.DateTimeField(auto_now_add=True)
+    active_time = models.IntegerField(default=1) #in hours
+    #Watchers
     isactive = models.BooleanField(default=True)
 
     def __str__(self):
         return f"title: {self.title} | minumum bid: {self.min_bid} | is active: {self.isactive}"
 
 
-"""
-Example model:
-
-from django.db import models
-
-# Create your models here.
-class Airport(models.Model):
-    code = models.CharField(max_length=3)
-    city = models.CharField(max_length=64)
+class bid(models.Model):
+    time_create = models.DateTimeField(auto_now_add=True)
+    #bidder = request.user
+    listing = models.ForeignKey(listing, on_delete=models.CASCADE, related_name="bids")
+    bid = models.DecimalField(max_digits=5, decimal_places=0, default=1)
 
     def __str__(self):
-        return f"{self.city} ({self.code})"
+        return f"listing: {self.listing} | bid: {self.bid}"
 
 
-class Flight(models.Model):
-    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
-    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
-    duration = models.IntegerField()
-
+class comment(models.Model):
+    time_create = models.DateTimeField(auto_now_add=True)
+    #bidder = request.user
+    listing = models.ForeignKey(listing, on_delete=models.CASCADE, related_name="comments")
+    comment = models.CharField(max_length=100) 
+    
     def __str__(self):
-        return f"{self.id} : {self.origin} to {self.destination}"
-
-
-class Passenger(models.Model):
-    first = models.CharField(max_length=64)
-    last = models.CharField(max_length=64)
-    flights = models.ManyToManyField(Flight, blank=True, related_name="passengers")
-
-    def __str__(self):
-        return f"{self.first} {self.last}"
-"""
+        return f"listing: {self.listing} | bid: {self.comment}"
