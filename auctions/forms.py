1cb401c2bccba85e80560ee1b3e4b252001f8661
Adam Knapp <adamfknapp@gmail.com>
2021-03-07 21:09:02 -0500

    pycodestyle
    cleaning with pycode style

--- auctions/forms.py
+++ auctions/forms.py
@@ -1,15 +1,18 @@
 from django.forms import ModelForm
 from .models import listing, comment, bid
 
+
 class listing_form(ModelForm):
     class Meta:
         model = listing
         fields = ["title", "description", "photo", "category", "start_bid"]
 
+
 class comment_form(ModelForm):
     class Meta:
         model = comment
         fields = ['comment']
+
 
 class bid_form(ModelForm):
     class Meta:
